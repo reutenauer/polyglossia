66116d4a36c7a5d401f9df8630477b0367aabb16
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2012-04-29 04:44:29 +0100

    Test routine’s output a little more user-friendly.

--- tests/run.lua
+++ tests/run.lua
@@ -13,13 +13,10 @@ if not match(slash, ego) then -- Path is not absolute
   abspath = lfs.currentdir() .. '/' .. ego
 end
 
-print(abspath)
-
 -- TODO no idea why it doesn’t work the other way round: C((slash * nonslash^1)^1) * slash
 local dirnamepatt = C(slash * (nonslash^1 * slash)^0)
 
 local testdir = match(dirnamepatt, abspath)
-print(testdir)
 
 -- TODO I don’t like that at all ...
 local alnum = lpeg.S'-_' + lpeg.R'az' + lpeg.R'AZ'
@@ -31,8 +28,27 @@ local outdir = testdir .. 'out'
 lfs.mkdir(outdir) -- returns nil and string 'File exists' if it exists already.
 lfs.chdir(outdir)
 
+local basenames = { }
 for tex in lfs.dir(testdir) do
   if match(dottex, tex) then
-    os.execute("xelatex " .. testdir .. '/' .. tex)
+    local basename = match(C(alnum^0), tex)
+    table.insert(basenames, basename)
+  end
+end
+
+local errors = { }
+
+-- Designed for Lua 5.1 (see _VERSION).  os.execute returns only the command’s return value.
+for _, basename in ipairs(basenames) do
+  local tex = basename .. '.tex'
+  if match(dottex, tex) then
+    os.execute("xelatex " .. testdir .. '/' .. tex .. ' >/dev/null')
+    os.execute("pdftotext -enc UTF-8 " .. outdir .. '/' .. basename ..  '.pdf' .. ' >/dev/null')
+    local retvalue = os.execute("diff " .. testdir .. '/ref/' .. basename .. '.txt ' ..  testdir .. '/out/' .. basename .. '.txt')
+    if(retvalue == 0) then
+      print('Test file ' .. tex .. ' OK.')
+    else
+      print('Something went wrong with ' .. tex)
+    end
   end
 end
