9552f235ffa9d1fb425c8b705628cd95dad37bf5
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2012-04-29 04:08:12 +0100

    Made run.lua a little more robust.

--- tests/run.lua
+++ tests/run.lua
@@ -1,30 +1,38 @@
 require 'lfs'
-require'lpeg'
+require 'lpeg'
 
-local P, match = lpeg.P, lpeg.match
+local P, C, match = lpeg.P, lpeg.C, lpeg.match
 
 local slash = P'/'
-local dot = P'.'
+local nonslash = 1 - slash
 
-local dir = arg[0]
-local testdir = dir
+local ego = arg[0]
+local abspath = ego
 
-if not match(slash, dir) then -- Path is not absolute
-  if match(dot, dir) then -- Path is relative
-                          -- and can be appended to current directory
-    testdir = lfs.currentdir() .. '/' .. dir
-  else -- testdir is current directory
-    testdir = lfs.currentdir()
-  end
+if not match(slash, ego) then -- Path is not absolute
+  abspath = lfs.currentdir() .. '/' .. ego
 end
 
--- local dottex = (P(1) + P'-')^0 * P'.tex' * -1 -- Doesn’t work
--- local dottex = lpeg.R'az' * P'.tex' * -1 -- Doesn’t work
-local dottex = (lpeg.S'-_' + lpeg.R'az' + lpeg.R'AZ')^0 * P'.tex' * -1
--- local dottex = lpeg.R'!~'^0 * P'.tex' * -1 -- Doesn’t work
+print(abspath)
+
+-- TODO no idea why it doesn’t work the other way round: C((slash * nonslash^1)^1) * slash
+local dirnamepatt = C(slash * (nonslash^1 * slash)^0)
+
+local testdir = match(dirnamepatt, abspath)
+print(testdir)
+
+-- TODO I don’t like that at all ...
+local alnum = lpeg.S'-_' + lpeg.R'az' + lpeg.R'AZ'
+local dottex = alnum^0 * P'.tex' * -1
+
+-- lfs.chdir(testdir + 'out')
+
+local outdir = testdir .. 'out'
+lfs.mkdir(outdir) -- returns nil and string 'File exists' if it exists already.
+lfs.chdir(outdir)
 
 for tex in lfs.dir(testdir) do
   if match(dottex, tex) then
-    os.execute("xelatex " .. tex)
+    os.execute("xelatex " .. testdir .. '/' .. tex)
   end
 end
