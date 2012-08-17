d4fa87929e6f9ed8d314666c2ab48f49d717c92a
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2012-08-17 04:41:58 +0100

    Refactor and extend run.lua.  Not all the new features work yet, but it’s late and I’m being attacked by moths.

--- tests/run.lua
+++ tests/run.lua
@@ -11,8 +11,53 @@ local nonslash = 1 - slash
 local ego = arg[0]
 local abspath = ego
 
+--[===[
+We may provide a couple of switches on the command line, see usage.
+
+TODO: add the possibility to run on a list of directories too.
+]===]
+
+usage = [==[
+Usage:
+	<ego> [-h | -x | -l] [file1[, file2, etc.]]
+
+  -h prints a help message
+  -x uses XeTeX exclusively
+  -l uses LuaTeX exclusively
+  (Default: both)
+
+  The remainder of the arguments is a list of files: we run them
+  consecutively (instead of the content of the directory containing
+  <ego>)
+]==]
+
+-- Resolve absolute path of <ego>
+local currdir = lfs.currentdir()
 if not match(slash, ego) then -- Path is not absolute
-  abspath = lfs.currentdir() .. '/' .. ego
+  abspath = currdir .. '/' .. ego
+end
+
+
+-- Parse command-line arguments
+local arg1 = arg[1]
+
+formats = { 'xelatex', 'lualatex' }
+files = { }
+
+if arg1 == '-h' then
+  print(usage)
+  return
+elseif arg1 == '-x' then
+  formats = { 'xelatex' }
+elseif arg1 == '-l' then
+  formats = { 'lualatex' }
+elseif #arg > 0 then -- arg is a list of files
+  -- TODO does not yet work.
+  if not match(slash, ego) then -- Path is not absolute
+    for _, f in ipairs(arg) do
+      table.insert(files, currdir .. '/' .. f)
+    end
+  end
 end
 
 -- TODO no idea why it doesn’t work the other way round: C((slash * nonslash^1)^1) * slash
@@ -41,28 +86,39 @@ for tex in lfs.dir(testdir) do
 end
 
 local errors = { }
+for _, format in ipairs(formats) do
+  errors[format] = { }
+end
 
 -- TODO TODO TODO Test with LuaTeX as well, now!
 -- Designed for Lua 5.1 (see _VERSION).  os.execute returns only the command’s return value.
-for _, basename in ipairs(basenames) do
-  local tex = basename .. '.tex'
-  if match(dottex, tex) then
-    os.execute("xelatex " .. testdir .. '/' .. tex)
-    os.execute("pdftotext -layout -enc UTF-8 " .. outdir .. '/' .. basename ..  '.pdf' .. ' >/dev/null')
-    local retvalue = os.execute("diff " .. testdir .. '/ref/' .. basename .. '.txt ' ..  testdir .. '/out/' .. basename .. '.txt')
-    if(retvalue == 0) then
-      errors[tex] = false
-      print('Test file ' .. tex .. ' OK.')
-    else
-      errors[tex] = true
-      print('Something went wrong with ' .. tex)
+if #files > 0 then
+  -- TODO (in relation with the “does not yet work” above)
+else
+  for _, format in ipairs(formats) do
+    for _, basename in ipairs(basenames) do
+      local tex = basename .. '.tex'
+      if match(dottex, tex) then
+        os.execute(format .. " " .. testdir .. '/' .. tex)
+        os.execute("pdftotext -layout -enc UTF-8 " .. outdir .. '/' .. basename ..  '.pdf' .. ' >/dev/null')
+        local retvalue = os.execute("diff " .. testdir .. '/ref/' .. basename .. '.txt ' ..  testdir .. '/out/' .. basename .. '.txt')
+        if(retvalue == 0) then
+          errors[format][tex] = false
+          print('Test file ' .. tex .. ' OK.')
+        else
+          errors[format][tex] = true
+          print('Something went wrong with ' .. tex)
+        end
+      end
     end
   end
 end
 
 local success = true
-for _, err in pairs(errors) do
-  if err then success = false; break end
+for form, formerrs in pairs(errors) do
+  for _, err in pairs(formerrs) do
+    if err then success = false; break end
+  end
 end
 
 print()
@@ -71,9 +127,15 @@ if success then
   print('All tests passed.')
 else
   print('Some tests are failing.  There were problems with the following files: ')
-  for tex, err in pairs(errors) do
-    if err then
-      print(tex)
+  for form, formerrs in pairs(errors) do
+    print('---- ' .. form)
+    for tex, err in pairs(formerrs) do
+      if err then
+        print(tex)
+      end
     end
   end
+  print('----')
+  print()
+  print('Sorry!')
 end
