bfa6b35189a47b0a09e79e15d62f9cd8160e3c6a
Elie Roux <elie.roux@telecom-bretagne.eu>
2013-04-23 22:49:36 +0200

    Improving straightforwardness

--- lua/xpg-hyphen.lua
+++ lua/xpg-hyphen.lua
@@ -1,33 +1,15 @@
-local dofile = dofile
-local kpse, texio = kpse, texio
-dofile(kpse.find_file('luatex-hyphen.lua'))
+require('luatex-hyphen')
+
 local luatexhyphen = luatexhyphen
 
-module('xpg_hyphen')
--- kpse.set_program_name('lualatex', 'lualatex')
+xpg_hyphen = xpg_hyphen or {}
 
-local language_dat_filename = kpse.find_file('language.dat.lua')
-local language_dat
-
-if language_dat_filename then
-  language_dat = dofile(language_dat_filename)
-else
-  language_dat = { }
-end
-
-function findlang(lang)
-  return language_dat[lang]
-end
-
-function loadlang(lang, id)
-  texio.write_nl('term and log', 'DEBUG: lang = ' .. lang .. ', id = ' .. id)
-  if lang == 'english' then
-    texio.write_nl('term and log', 'Not loading patterns for language 0; it should be dumped in the format')
-  else
-    texio.write_nl('term and log', "Trying to load patterns for language " ..  lang .. ", id " .. id)
-
-    if findlang(lang) then
-      luatexhyphen.loadlanguage(lang, id)
-    end
+local function loadlang(lang, id)
+  texio.write_nl("id: "..id.."\nlang: "..lang)
+  if (id) and not luatexhyphen.lookupname(lang) then
+    luatexhyphen.loadlanguage(lang, id) 
+    texio.write_nl()
   end
 end
+
+xpg_hyphen.loadlang = loadlang
