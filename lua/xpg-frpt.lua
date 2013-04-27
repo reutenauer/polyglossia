557f9adac2f16577a33bbb66ec4d91d0496fc5f9
Elie Roux <elie.roux@telecom-bretagne.eu>
2013-04-27 19:49:25 +0200

    Trying with nodes... (not finished yet)

--- lua/xpg-frpt.lua
+++ lua/xpg-frpt.lua
@@ -1,160 +1,186 @@
 local add_to_callback = luatexbase.add_to_callback
 local remove_from_callback = luatexbase.remove_from_callback
 local priority_in_callback = luatexbase.priority_in_callback
-local tokencreate = token.create
-local mathfloor = math.floor
-local tokenis_expandable = token.is_expandable
-local tokenis_activechar = token.is_activechar
-local tokenget_next = token.get_next
-local texcount = tex.count
-local tokencommand_name = token.command_name
 
-local module = {
-    name          = "xpg-frpt",
-    version       = 0.2,
-    date          = "2013/04/23",
-    description   = "Companion to Polyglossia for french punctuation",
-    author        = "Elie Roux",
-    copyright     = "Elie Roux",
-    license       = "CC0"
-}
+local next, type = next, type
+local utfchar = utf.char
 
-local error, warning, info, log =
-    luatexbase.provides_module(module)
+local nodes, fonts, node = nodes, fonts, node
 
-xpg_frpt = xpg_frpt or {}
+local insert_node_before = node.insert_before
+local insert_node_after  = node.insert_after
+local remove_node        = nodes.remove
+local end_of_math        = node.end_of_math
+local has_attribute      = node.has_attribute
 
--- This mimics XeTeXintercharclasses, and is a tuned version of
--- http://wiki.luatex.org/index.php?title=Token_filter
-
--- We have four intercharclasses, we make a hash with
---   character -> intercharclass
--- In order to mimic the behaviour of XeTeX, we don't use class 0-3, we consider
--- that class 0 is the one of "normal" tokens, and that class 255 is the one of
--- text string boudaries (glue, kern, math, box, etc.).
--- 
--- Our new classes are (with the same names as in gloss-french.ldf:
---   * 4: french@punctthin
---   * 5: french@punctthick
---   * 6: french@punctguillstart
---   * 7: french@punctguillend
-
-local charclasses = {
-  [string.byte(':')] = 4,
-  [string.byte('?')] = 5, [string.byte('!')] = 5, [string.byte(';')] = 5, [string.byte('‼')] = 5, 
-  [string.byte('⁇')] = 5, [string.byte('⁈')] = 5, [string.byte('⁉')] = 5,
-  [string.byte('«')] = 6, [string.byte('<')] = 6, --poses many problems
-  [string.byte('»')] = 7, [string.byte('>')] = 7, --idem
-  -- naive approach, we also put in this category anything with catcode != 11 or 12
-  [string.byte(' ')] = 255, [string.byte(' ')] = 255,
-  }
-  
--- Here we have commented out > and < characters. That's because token_filter
--- get fed with *everything*, including things like \ifdim\foo>0pt, which would
--- make the function insert a token and would give something like 
--- \ifdim\foo\the\toksxx>0pt and break everything! I'm not even sure it can be
--- worked around... The best would be to change all this and work with nodes,
--- a much more reliable solution!
-
--- we have 5 possible toks to insert, we reference them in a table
--- corresponding to the arguments of XeTeXinterchartoks
--- the five toks are defined in gloss-french.ldf
-local intercharclasses_toksnumbers = {
-  [0] = {}, [255] = {}, [6] = {}, [7] = {}, [4] = {}}
-
--- It's better to use \the\toksxx than the name of the toks, so we create newtoks
--- in the .def file and feed lua with their allocationnumber
-local map_toks = {}
-local function map_toks_number (allocationnumber, toksnumber)
-  map_toks[toksnumber] = allocationnumber
-end
-
--- one we're done with newtoks, we can create the final table
-local function create_intercharclasses_toksnumbers()
-  intercharclasses_toksnumbers[0][4]   = map_toks[1] -- 'xpg@lu@toks@one'  {\nobreak\thinspace}%
-  intercharclasses_toksnumbers[0][5]   = map_toks[2] -- 'xpg@lu@toks@two'  {\nobreakspace}%
-  intercharclasses_toksnumbers[255][4] = map_toks[3] -- 'xpg@lu@toks@three'{\xpg@unskip\nobreak\thinspace}%
-  intercharclasses_toksnumbers[255][5] = map_toks[4] -- 'xpg@lu@toks@four' {\xpg@unskip\nobreakspace}%
-  intercharclasses_toksnumbers[6][0]   = map_toks[2] -- 'xpg@lu@toks@two'  {\nobreakspace}% "«a" -> "« a"
-  intercharclasses_toksnumbers[0][7]   = map_toks[2] -- 'xpg@lu@toks@two'  {\nobreakspace}% "a»" -> "a »"
-  intercharclasses_toksnumbers[6][255] = map_toks[5] -- 'xpg@lu@toks@five' {\nobreakspace\xpg@nospace}% "«  " -> "«~"
-  intercharclasses_toksnumbers[255][7] = map_toks[4] -- 'xpg@lu@toks@four' {\xpg@unskip\nobreakspace}% "  »" -> "~»"
-  intercharclasses_toksnumbers[7][4]   = map_toks[1] -- 'xpg@lu@toks@one'  {\nobreak\thinspace}% "»;" -> "» ;"
-  intercharclasses_toksnumbers[7][5]   = map_toks[2] -- 'xpg@lu@toks@two'  {\nobreakspace}% "»:" -> "» :"
-  intercharclasses_toksnumbers[4][7]   = map_toks[2] -- 'xpg@lu@toks@two'  {\nobreakspace}% "?»" -> "? »"
-end
-  
--- we store the previous character class
-local previous_charclass
--- initalizing it to 255 seems safe
-previous_charclass = 255
+local texattribute       = tex.attribute
 
 
-local function activate()
-  if not priority_in_callback ("token_filter", "xpg-frpt.do_intertoks") then
-    add_to_callback("token_filter", do_intertoks, "xpg-frpt.do_intertoks")
-  end
-end
+local nodepool           = nodes.pool
+local tasks              = nodes.tasks
 
-local function desactivate()
-  if priority_in_callback ("token_filter", "xpg-frpt.do_intertoks") then
-    remove_from_callback("token_filter", "xpg-frpt.do_intertoks")
-  end
-end
+local new_penalty        = nodepool.penalty
+local new_glue           = nodepool.glue
 
-function do_intertoks () 
-  local tok = tokenget_next() 
-  local newcharclass -- the char class of the current token
-  -- an additional security, not sure if it's useful
-  local commandname = tokencommand_name(tok)
-  if texcount['xpg@interchartokenstate'] == 1 then 
-      if tokenis_expandable(tok) or tokenis_activechar(tok) then
-      --  previous_charclass = 254 -- we consider it's "invalid"
-      --  return tok
-      --end
-      if tok[1] == 11 or  tok[1] == 12 then
-        -- if catcode is letter or other, then fine
-        newcharclass = charclasses[tok[2]] or 0
-        if commandname == 'spacer' then
-          newcharclass = 255
+
+-- node types as of April 2013
+local glue_code = 10
+local glyph_code = 37
+local penalty_code = 12
+local kern_code = 11
+
+-- we have here all possible space characters, referenced by their
+-- unicode slot number, taken from char-def.lua
+local space_chars = {20=1, 160=1, 5760=1, 6158=1, 8192=1, 8193=1, 8194=1, 8195=1, 
+  8196=1, 8197=1, 8198=1, 8199=1, 8200=1, 8201=1, 8202=1, 8239=1, 8287=1, 12288=1}
+
+-- from nodes-tst.lua, adapted
+function nodes.somespace(n,all)
+    if n then
+        local id = n.id
+        if id == glue_code then
+            return (all or (n.spec.width ~= 0)) and glue_code
+        elseif id == kern_code then
+            return (all or (n.kern ~= 0)) and kern_code
+        elseif id == glyph_code then
+            if space_char[n.char] then
+                return true
+            else
+            return glyph_code
         end
-      else 
-        -- else we consider we're at a boundary of a text string
-        newcharclass = 255
-      end
-      --texio.write_nl(unicode.utf8.char(tok[2])..' '..commandname)
-      --texio.write_nl("previous_charclass: "..previous_charclass.."\nnewcharclass: "..newcharclass)
-      local toks_to_insert = intercharclasses_toksnumbers[previous_charclass] and 
-         intercharclasses_toksnumbers[previous_charclass][newcharclass]
-      if toks_to_insert then
-          local h = mathfloor(toks_to_insert / 100)
-          local d = mathfloor((toks_to_insert - h*100) / 10)
-          local u = toks_to_insert % 10
-          --texio.write_nl(token.command_name(tok))
-          tok = {
-            -- \xpg@interchartokenstate=0 \the\toks<n> \xpg@interchartokenstate=1 tok
-            token.create('xpg@interchartokenstate'),
-            token.create(string.byte('='),12),
-            token.create(string.byte(' '),10),
-            token.create(string.byte('0'),12),
-            token.create('relax'),
-            tokencreate('the'),
-            tokencreate('toks'),
-            h and tokencreate(string.byte(h),12),
-            d and tokencreate(string.byte(d),12),
-            tokencreate(string.byte(u),12),
-            tokencreate(string.byte(' '),10),
-            token.create('xpg@interchartokenstate'),
-            token.create(string.byte('='),12),
-            token.create(string.byte(' '),10),
-            token.create(string.byte('1'),12),
-            token.create('relax'),
-              {tok[1], tok[2], tok[3]}}               
-      end
-      previous_charclass = newcharclass
-  end
-  return tok
+    end
+    return false
 end
+
+texio.write_nl(table.serialize(node.types()))
+
+-- idem
+function nodes.somepenalty(n,value)
+    if n then
+        local id = n.id
+        if id == penalty_code then
+            if value then
+                return n.penalty == value
+            else
+                return true
+            end
+        end
+    end
+    return false
+end
+
+local somespace          = nodes.somespace
+local somepenalty        = nodes.somepenalty
+
+local xpgfrptattr = tex.attribute['xpg@frpt']
+texio.write_nl(xpgfrptattr)
+
+local left=1
+local right=2
+
+local mappings =  {
+ [':'] = {left, 0.25},
+ ['!'] = {left, 0.25},
+ ['?'] = {left, 0.25},
+ [';'] = {left, 0.25},
+ ['‼'] = {left, 0.25},
+ ['⁇'] = {left, 0.25},
+ ['⁈'] = {left, 0.25},
+ ['⁉'] = {left, 0.25},
+ ['»'] = {left, 0.25},
+ ['>'] = {left, 0.25},
+ ['«'] = {left, 0.25}, 
+ ['‹'] = {left, 0.25}, 
+ }
+ 
+local spacing = 0.25
+
+-- from typo-spa.lua
+local function process(head)
+    local done = false
+    local start = head
+    -- head is always begin of par (whatsit), so we have at least two prev nodes
+    -- penalty followed by glue
+    while start do
+        local id = start.id
+        if id == glyph_code then -- 37 is glyph as of 2013/04
+            local attr = has_attribute(start, xpgfrptattr)
+            if attr and attr > 0 then
+                local char = start.char
+                local map = mappings[char]
+                --node.unset_attribute(start, xpgfrptattr) -- needed?
+                if map then
+                    local quad = font.fonts[start.font].parameters.quad -- might be optimized
+                    local prev = start.prev
+                    if map[1] == left and prev then
+                        local prevprev = prev.prev
+                        local somespace = somespace(prev,true)
+                        if somespace then
+                            local somepenalty = somepenalty(prevprev,10000)
+                            if somepenalty then
+                                if trace_spacing then
+                                    report_spacing("removing penalty and space before %C (left)",char)
+                                end
+                                head = remove_node(head,prev,true)
+                                head = remove_node(head,prevprev,true)
+                            else
+                                if trace_spacing then
+                                    report_spacing("removing space before %C (left)",char)
+                                end
+                                head = remove_node(head,prev,true)
+                            end
+                        end
+                        if trace_spacing then
+                            report_spacing("inserting penalty and space before %C (left)",char)
+                        end
+                        insert_node_before(head,start,new_penalty(10000))
+                        insert_node_before(head,start,new_glue(map[2]*quad))
+                        done = true
+                    end
+                    local next = start.next
+                    if map[1] == right and next then
+                        local nextnext = next.next
+
+                        local somepenalty = somepenalty(next,10000)
+                        if somepenalty then
+                            local somespace = somespace(nextnext,true)
+                            if somespace then
+                                if trace_spacing then
+                                    report_spacing("removing penalty and space after %C right",char)
+                                end
+                                head = remove_node(head,next,true)
+                                head = remove_node(head,nextnext,true)
+                            end
+                        else
+                            local somespace = somespace(next,true)
+                            if somespace then
+                                if trace_spacing then
+                                    report_spacing("removing space after %C (right)", char)
+                                end
+                                head = remove_node(head,next,true)
+                            end
+                        end
+                        if trace_spacing then
+                            report_spacing("inserting penalty and space after %C (right)",char)
+                        end
+                        insert_node_after(head,start,new_glue(right*quad))
+                        insert_node_after(head,start,new_penalty(10000))
+                        done = true
+                    end
+                end
+            end
+        elseif id == math_code then
+            -- warning: this is a feature of luatex > 0.76
+            start = end_of_math(start) -- weird, can return nil .. no math end?
+        end
+        if start then
+            start = start.next
+        end
+    end
+    return head, done
+end
+
+add_to_callback("pre_linebreak_filter", process, "xpg-frpt.process")
 
 xpg_frpt.get_toks_number = get_toks_number
 xpg_frpt.activate = activate
