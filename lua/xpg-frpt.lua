ea5d6d7bddef509b8d7da9c73dbda80281e6b979
Elie Roux <elie.roux@telecom-bretagne.eu>
2013-04-28 16:01:08 +0200

    Now it works, except for the last line...

--- lua/xpg-frpt.lua
+++ lua/xpg-frpt.lua
@@ -1,3 +1,19 @@
+local module = {
+    name = "xpg-frpt",
+    version = 0.2,
+    date = "2013/04/23",
+    description = "Companion to Polyglossia for french punctuation",
+    author = "Elie Roux",
+    copyright = "Elie Roux",
+    license = "CC0"
+}
+
+local error, warning, info, log =
+    luatexbase.provides_module(module)
+
+xpg_frpt = xpg_frpt or {}
+
+
 local add_to_callback = luatexbase.add_to_callback
 local remove_from_callback = luatexbase.remove_from_callback
 local priority_in_callback = luatexbase.priority_in_callback
@@ -24,18 +40,37 @@ local new_glue           = nodepool.glue
 
 
 -- node types as of April 2013
-local glue_code = 10
-local glyph_code = 37
-local penalty_code = 12
-local kern_code = 11
+local glue_code     = 10
+local glue_spec_code= 47
+local glyph_code    = 37
+local penalty_code  = 12
+local kern_code     = 11
+
+-- we make a new node, so that we can copy it later on
+local penalty_node  = node.new(penalty_code)
+penalty_node.penalty = 10000
+
+local function get_penalty_node()
+  return node.copy(penalty_node)
+end
+
+-- same for glue node
+local glue_node  = node.new(glue_code)
+glue_node.spec = node.new(glue_spec_code)
+
+local function get_glue_node(dim)
+  local n = node.copy(glue_node)
+  n.spec.width = dim
+  return n
+end
 
 -- we have here all possible space characters, referenced by their
 -- unicode slot number, taken from char-def.lua
-local space_chars = {20=1, 160=1, 5760=1, 6158=1, 8192=1, 8193=1, 8194=1, 8195=1, 
-  8196=1, 8197=1, 8198=1, 8199=1, 8200=1, 8201=1, 8202=1, 8239=1, 8287=1, 12288=1}
+local space_chars = {[20]=1, [160]=1, [5760]=1, [6158]=1, [8192]=1, [8193]=1, [8194]=1, [8195]=1, 
+  [8196]=1, [8197]=1, [8198]=1, [8199]=1, [8200]=1, [8201]=1, [8202]=1, [8239]=1, [8287]=1, [12288]=1}
 
 -- from nodes-tst.lua, adapted
-function nodes.somespace(n,all)
+local function somespace(n,all)
     if n then
         local id = n.id
         if id == glue_code then
@@ -43,19 +78,18 @@ function nodes.somespace(n,all)
         elseif id == kern_code then
             return (all or (n.kern ~= 0)) and kern_code
         elseif id == glyph_code then
-            if space_char[n.char] then
+            if space_chars[n.char] then
                 return true
             else
-            return glyph_code
+                return false
+            end
         end
     end
     return false
 end
 
-texio.write_nl(table.serialize(node.types()))
-
 -- idem
-function nodes.somepenalty(n,value)
+local function somepenalty(n,value)
     if n then
         local id = n.id
         if id == penalty_code then
@@ -69,31 +103,37 @@ function nodes.somepenalty(n,value)
     return false
 end
 
-local somespace          = nodes.somespace
-local somepenalty        = nodes.somepenalty
-
-local xpgfrptattr = tex.attribute['xpg@frpt']
-texio.write_nl(xpgfrptattr)
+local xpgfrptattr = luatexbase.attributes['xpg@frpt']
+--texio.write_nl(xpgfrptattr)
 
 local left=1
 local right=2
+local byte = unicode.utf8.byte
+
+local spacing = 0.16 -- \thinspace is 1/6 of em
 
 local mappings =  {
- [':'] = {left, 0.25},
- ['!'] = {left, 0.25},
- ['?'] = {left, 0.25},
- [';'] = {left, 0.25},
- ['‼'] = {left, 0.25},
- ['⁇'] = {left, 0.25},
- ['⁈'] = {left, 0.25},
- ['⁉'] = {left, 0.25},
- ['»'] = {left, 0.25},
- ['>'] = {left, 0.25},
- ['«'] = {left, 0.25}, 
- ['‹'] = {left, 0.25}, 
+ [byte(':')] = {left, 0.16},
+ [byte('!')] = {left, 0.16},
+ [byte('?')] = {left, 0.16},
+ [byte(';')] = {left, 0.16},
+ [byte('‼')] = {left, 0.16},
+ [byte('⁇')] = {left, 0.16},
+ [byte('⁈')] = {left, 0.16},
+ [byte('⁉')] = {left, 0.16},
+ [byte('»')] = {left, 0.16},
+ [byte('>')] = {left, 0.16},
+ [byte('«')] = {right, 0.16}, 
+ [byte('‹')] = {right, 0.16}, 
  }
- 
-local spacing = 0.25
+
+local function set_spacing(ems) -- em size
+  if ems == spacing then return end
+  spacing = ems
+  for _, m in pairs(mappings) do
+    m[2] = ems
+  end
+end
 
 -- from typo-spa.lua
 local function process(head)
@@ -118,23 +158,14 @@ local function process(head)
                         if somespace then
                             local somepenalty = somepenalty(prevprev,10000)
                             if somepenalty then
-                                if trace_spacing then
-                                    report_spacing("removing penalty and space before %C (left)",char)
-                                end
                                 head = remove_node(head,prev,true)
                                 head = remove_node(head,prevprev,true)
                             else
-                                if trace_spacing then
-                                    report_spacing("removing space before %C (left)",char)
-                                end
                                 head = remove_node(head,prev,true)
                             end
                         end
-                        if trace_spacing then
-                            report_spacing("inserting penalty and space before %C (left)",char)
-                        end
-                        insert_node_before(head,start,new_penalty(10000))
-                        insert_node_before(head,start,new_glue(map[2]*quad))
+                        insert_node_before(head,start,get_penalty_node())
+                        insert_node_before(head,start,get_glue_node(map[2]*quad))
                         done = true
                     end
                     local next = start.next
@@ -145,26 +176,17 @@ local function process(head)
                         if somepenalty then
                             local somespace = somespace(nextnext,true)
                             if somespace then
-                                if trace_spacing then
-                                    report_spacing("removing penalty and space after %C right",char)
-                                end
                                 head = remove_node(head,next,true)
                                 head = remove_node(head,nextnext,true)
                             end
                         else
                             local somespace = somespace(next,true)
                             if somespace then
-                                if trace_spacing then
-                                    report_spacing("removing space after %C (right)", char)
-                                end
                                 head = remove_node(head,next,true)
                             end
                         end
-                        if trace_spacing then
-                            report_spacing("inserting penalty and space after %C (right)",char)
-                        end
-                        insert_node_after(head,start,new_glue(right*quad))
-                        insert_node_after(head,start,new_penalty(10000))
+                        insert_node_after(head,start,get_glue_node(right*quad))
+                        insert_node_after(head,start,get_penalty_node())
                         done = true
                     end
                 end
@@ -180,10 +202,19 @@ local function process(head)
     return head, done
 end
 
-add_to_callback("pre_linebreak_filter", process, "xpg-frpt.process")
+local function activate()
+  if not priority_in_callback ("pre_linebreak_filter", "xpg-frpt.process") then
+    add_to_callback("pre_linebreak_filter", process, "xpg-frpt.process")
+  end
+end
 
-xpg_frpt.get_toks_number = get_toks_number
-xpg_frpt.activate = activate
+local function desactivate()
+  if priority_in_callback ("pre_linebreak_filter", "xpg-frpt.process") then
+    remove_from_callback("pre_linebreak_filter", "xpg-frpt.process")
+  end
+end
+
+xpg_frpt.activate    = activate
 xpg_frpt.desactivate = desactivate
-xpg_frpt.map_toks_number = map_toks_number 
-xpg_frpt.create_intercharclasses_toksnumbers = create_intercharclasses_toksnumbers
+xpg_frpt.set_spacing = set_spacing
+xpg_frpt.spacing     = spacing
