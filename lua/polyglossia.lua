93a13c64bacf919a402c83e55d94287e138f6e5d
Elie Roux <elie.roux@telecom-bretagne.eu>
2013-04-29 16:14:21 +0200

    A little more things in lua

--- lua/polyglossia.lua
+++ lua/polyglossia.lua
@@ -3,10 +3,10 @@ require('luatex-hyphen')
 local luatexhyphen = luatexhyphen
 
 local module = {
-    name          = "xpg-hyphen",
+    name          = "polyglossia",
     version       = 0.2,
     date          = "2013/04/23",
-    description   = "Companion to Polyglossia",
+    description   = "Polyglossia",
     author        = "Elie Roux",
     copyright     = "Elie Roux",
     license       = "CC0"
@@ -15,7 +15,10 @@ local module = {
 local error, warning, info, log =
     luatexbase.provides_module(module)
 
-xpg_hyphen = xpg_hyphen or {}
+polyglossia = polyglossia or {}
+
+local current_language
+local default_language
 
 local function loadlang(lang, id)
   if id and luatexhyphen.lookupname(lang) then
@@ -23,4 +26,17 @@ local function loadlang(lang, id)
   end
 end
 
-xpg_hyphen.loadlang = loadlang
+local function select_language(lang, id)
+  loadlang(lang, id)
+  current_language = lang
+end
+
+local function set_default_language(lang, id)
+  default_language = lang
+end
+
+polyglossia.loadlang = loadlang
+polyglossia.select_language = select_language
+polyglossia.set_default_language = set_default_language
+polyglossia.current_language = current_language
+polyglossia.default_language = default_language
