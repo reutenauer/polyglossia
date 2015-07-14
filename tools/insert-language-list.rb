0db1f91bd9263e7cc68ba3d51800ec21bea988d0
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2015-07-14 20:48:43 +0100

    Finished script.

--- tools/insert-language-list.rb
+++ tools/insert-language-list.rb
@@ -1,32 +1,37 @@
 #!/usr/bin/env ruby
 require 'csv'
 
-preamble = <<__EOPreamble__
+def make_table
+  preamble = <<-__EOPreamble__
 \\begin{tabular}{lllll}
 \\hline
-__EOPreamble__
-postamble = <<__EOPostamble__
+  __EOPreamble__
+  postamble = <<-__EOPostamble__
 \\hline
 \\end{tabular}
-__EOPostamble__
+  __EOPostamble__
 
-f = File.open(File.expand_path('../../doc/languages.csv', __FILE__))
+  f = File.open(File.expand_path('../../doc/languages.csv', __FILE__))
 # CSV.open(File.read(File.expand_path('../../doc/languages.csv', __FILE__))) do |csv|
-languages = []
-CSV.open(f) do |csv|
-  csv.each do |row|
-    if row.last == "true"
-      languages << "\\TX{#{row.first}}"
-    else
-      languages << row.first
+  languages = []
+  CSV.open(f) do |csv|
+    csv.each do |row|
+      if row.last == "true"
+        languages << "\\TX{#{row.first}}"
+      else
+        languages << row.first
+      end
     end
   end
-end
-f.close
+  f.close
 
-nrow = (languages.count.to_f / 5).ceil
-0.upto(nrow - 1) do |row|
-  tablecontent = 0.upto(4).map do |col|
-    languages[row + col * nrow]
-  end.join(' & ')
+  nrow = (languages.count.to_f / 5).ceil
+  preamble + 0.upto(nrow - 1).inject([]) do |table, row|
+    table << 0.upto(4).map do |col|
+      language = languages[row + col * nrow]
+      language + ' ' * (14 - language.length)
+    end.join(' & ') + '\\\\'
+  end.join("\n") + postamble
 end
+
+puts make_table
