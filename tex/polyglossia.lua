86f4f96c7e522e183bc2ea23b61a899e5573c475
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2016-02-10 09:15:08 +0000

    This may be the right way out.

--- tex/polyglossia.lua
+++ tex/polyglossia.lua
@@ -114,7 +114,7 @@ local function newloader(langentry)
             return lang.id(langobject)
         else
             texio.write_nl('term and log', 'Language ' .. langentry .. ' not found in language.dat.lua')
-            return nil
+            return 255
         end
     end
 end
