02e895b6285db0d418ba27343780cce698b22e18
Keno Wehr <keno.wehr@abgol.de>
2019-09-11 21:34:15 +0200

    Code simplification.
    Unused variables were deleted.
    It is not necessary to insert a penalty before a kern node
    because a kern node is never a break point.

--- tex/polyglossia-frpt.lua
+++ tex/polyglossia-frpt.lua
@@ -6,9 +6,9 @@ local priority_in_callback = luatexbase.priority_in_callback
 
 local get_quad = luaotfload.aux.get_quad -- needs luaotfload > 20130516
 
-local next, type = next, type
+local next = next
 
-local nodes, fonts, node = nodes, fonts, node
+local nodes, node = nodes, node
 
 local nodecodes          = nodes.nodecodes
 
@@ -31,22 +31,13 @@ if not end_of_math then -- luatex < .76
 end
 
 -- node types according to node.types()
-local glue_code         = nodecodes.glue
-local glue_spec_code    = nodecodes.glue_spec
-local glyph_code        = nodecodes.glyph
-local penalty_code      = nodecodes.penalty
-local kern_code         = nodecodes.kern
+local glue_code    = nodecodes.glue
+local glyph_code   = nodecodes.glyph
+local penalty_code = nodecodes.penalty
+local kern_code    = nodecodes.kern
 
 -- we make a new node, so that we can copy it later on
-local penalty_node   = new_node(penalty_code)
-penalty_node.penalty = 10000
-
-local function get_penalty_node()
-  return node_copy(penalty_node)
-end
-
--- same for glue node
-local kern_node       = new_node(kern_code)
+local kern_node = new_node(kern_code)
 
 local function get_kern_node(dim)
   local n = node_copy(kern_node)
@@ -56,17 +47,18 @@ end
 
 -- we have here all possible space characters, referenced by their
 -- unicode slot number, taken from char-def.lua
-local space_chars = {[32]=1, [160]=1, [5760]=1, [6158]=1, [8192]=1, [8193]=1, [8194]=1, [8195]=1, 
-  [8196]=1, [8197]=1, [8198]=1, [8199]=1, [8200]=1, [8201]=1, [8202]=1, [8239]=1, [8287]=1, [12288]=1}
+local space_chars = {[32] = true, [160] = true, [5760] = true, [6158] = true, [8192] = true, [8193] = true,
+  [8194] = true, [8195] = true, [8196] = true, [8197] = true, [8198] = true, [8199] = true, [8200] = true,
+  [8201] = true, [8202] = true, [8239] = true, [8287] = true, [12288] = true}
 
 -- from nodes-tst.lua, adapted
-local function somespace(n,all)
+local function somespace(n)
     if n then
         local id = n.id
         if id == glue_code then
-            return (all or (n.spec.width ~= 0)) and glue_code
+            return glue_code
         elseif id == kern_code then
-            return (all or (n.kern ~= 0)) and kern_code
+            return kern_code
         elseif id == glyph_code then
             if space_chars[n.char] then
                 return true
@@ -95,48 +87,21 @@ end
 
 local xpgfrptattr = luatexbase.attributes['xpg@frpt']
 
-local left=1
-local right=2
-local byte = unicode.utf8.byte
-
 -- Now there is a good question: how do we now, in lua, what a \thinspace is?
 -- In the LaTeX source (ltspace.dtx) it is defined as:
 -- \def\thinspace{\kern .16667em }. I see no way of seeing if it has been
 -- overriden or not... So we stick to this value.
-local thinspace  = 0.16667 
+local thinspace  = 0.16667
 -- thickspace is defined in amsmath.sty as:
 -- \renewcommand{\;}{\mspace+\thickmuskip{.2777em}}. Same problem as above, we
 -- stick to this fixed value.
 local thickspace = 0.2777 -- 5/18
 
-local mappings =  {
- [byte(':')] = {left,  thickspace}, --really?
- [byte('!')] = {left,  thinspace},
- [byte('?')] = {left,  thinspace},
- [byte(';')] = {left,  thinspace},
- [byte('‼')] = {left,  thinspace},
- [byte('⁇')] = {left,  thinspace},
- [byte('⁈')] = {left,  thinspace},
- [byte('⁉')] = {left,  thinspace},
- [byte('»')] = {left,  thinspace},
- [byte('›')] = {left,  thinspace},
- [byte('«')] = {right, thinspace}, 
- [byte('‹')] = {right, thinspace}, 
- }
+local left_space = {[':'] = thickspace, ['!'] = thinspace, ['?'] = thinspace, [';'] = thinspace, ['‼'] = thinspace,
+  ['⁇'] = thinspace, ['⁈'] = thinspace, ['⁉'] = thinspace, ['»'] = thinspace, ['›'] = thinspace}
+local right_space = {['«'] = thinspace, ['‹'] = thinspace}
 
-local function set_spacings(thinsp, thicksp)
-  for _, m in pairs(mappings) do
-    if m[2] == thinspace then
-      m[2] = thinsp
-    elseif m[2] == thickspace then
-      m[2] = thicksp
-    end
-  end
-  thickspace = thicksp
-  thinspace = thinsp
-end
-
--- from typo-spa.lua
+-- from typo-spa.lua, adapted
 local function process(head)
     local done = false
     local start = head
@@ -147,47 +112,38 @@ local function process(head)
         if id == glyph_code then
             local attr = has_attribute(start, xpgfrptattr)
             if attr and attr > 0 then
-                local char = start.char
-                local map = mappings[char]
-                --node.unset_attribute(start, xpgfrptattr) -- needed?
-                if map then
+                local char = utf8.char(start.char) -- requires Lua 5.3
+                if left_space[char] or right_space[char] then
                     local quad = get_quad(start.font) -- might be optimized
                     local prev = start.prev
-                    if map[1] == left and prev then
+                    if left_space[char] and prev then
                         local prevprev = prev.prev
-                        local somespace = somespace(prev,true)
+                        if somespace(prev) then
                         -- TODO: there is a question here: do we override a preceding space or not?...
-                        if somespace then
-                            local somepenalty = somepenalty(prevprev,10000)
-                            if somepenalty then
+                            if somepenalty(prevprev,10000) then
                                 head = remove_node(head,prev)
                                 head = remove_node(head,prevprev)
                             else
                                 head = remove_node(head,prev)
                             end
                         end
-                        insert_node_before(head,start,get_penalty_node())
-                        insert_node_before(head,start,get_kern_node(map[2]*quad))
+                        insert_node_before(head,start,get_kern_node(left_space[char]*quad))
                         done = true
                     end
                     local next = start.next
-                    if map[1] == right and next then
+                    if right_space[char] and next then
                         local nextnext = next.next
-                        local somepenalty = somepenalty(next,10000)
-                        if somepenalty then
-                            local somespace = somespace(nextnext,true)
-                            if somespace then
+                        if somepenalty(next,10000) then
+                            if somespace(nextnext) then
                                 head = remove_node(head,next)
                                 head = remove_node(head,nextnext)
                             end
                         else
-                            local somespace = somespace(next,true)
-                            if somespace then
+                            if somespace(next) then
                                 head = remove_node(head,next)
                             end
                         end
-                        insert_node_after(head,start,get_kern_node(map[2]*quad))
-                        insert_node_after(head,start,get_penalty_node())
+                        insert_node_after(head,start,get_kern_node(right_space[char]*quad))
                         done = true
                     end
                 end
@@ -219,6 +175,3 @@ end
 
 polyglossia.activate_frpt    = activate
 polyglossia.desactivate_frpt = desactivate
-polyglossia.set_spacings     = set_spacings
-polyglossia.thinspace        = thinspace
-polyglossia.thickspace       = thickpace
