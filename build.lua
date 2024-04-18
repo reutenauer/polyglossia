f1dfd1cac853ebb33ec960a98f754d4af6ce93b2
Udi-Fogiel <udifoglle@gmail.com>
2024-04-18 20:31:10 +0300

    consider fontmap files in l3install

--- build.lua
+++ build.lua
@@ -7,6 +7,12 @@ module = "polyglossia"
 stdengine    = "xetex"
 checkengines = {"xetex","luatex"}
 
-sourcefiledir = "tex"
-sourcefiles = {"*.def", "*.ldf", "*.sty", "*.tex", "*.lde", "*.lua"}
-installfiles = sourcefiles
+sourcefiles = {"tex/*.def", "tex/*.ldf", "tex/*.sty", "tex/*.tex", 
+		"tex/*.lde", "tex/*.lua", "fontmapping/*.map"}
+installfiles = {"*.def", "*.ldf", "*.sty", "*.tex", "*.lde", "*.lua", "*.map", "*.tec"}
+tdslocations = {
+	"fonts/misc/xetex/fontmapping/" .. module .. "/" .. "*.map",
+	"fonts/misc/xetex/fontmapping/" .. module .. "/" .. "*.tec"
+	}
+unpackfiles = {"*.map"}
+unpackexe = "teckit_compile"
