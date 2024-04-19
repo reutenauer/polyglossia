23d0825c634d262be6345dd3d24de473bb2c1d5f
Udi-Fogiel <udifoglle@gmail.com>
2024-04-19 15:32:03 +0300

    remove unused scripts

--- tools/upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-version="$1"
-announcement="$2"
-filename=polyglossia.zip
-
-curl -i \
-    -d "pkg=polyglossia&vers=$version&author=Arthur%20Reutenauer&email=arthur%2Ereutenauer%40normalesup%2Eorg&description=Polyglossia%20is%20an%20alternative%20to%20Babel%20for%20XeLaTeX%20and%20LuaLaTeX&ctanPath=%2Fmacros%2Flatex%2Fcontrib%2Fpolyglossia&type=announce&announcement=$announcement&license=lppl&file=`cat "filename"`" \
-    http://ctan.org/upload/save
